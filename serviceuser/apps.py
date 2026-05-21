f091554eb77ceb4669d2b78ac4493d16f35b3e32
23201162-ui <23201162@uap-bd.edu>
2026-05-21 06:05:08 +0600

    deleted serviceuser

--- serviceuser/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-class ServiceuserConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'serviceuser'
-
-    def ready(self):
-        # The dot (.) means "this current folder"
-        from . import signals
--- serviceuser/signals.py
+++
@@ -1,9 +0,0 @@
-from django.db.models.signals import post_save
-from django.contrib.auth.models import User
-from django.dispatch import receiver
-from .models import ServiceUserProfile
-
-@receiver(post_save, sender=User)
-def create_service_user_profile(sender, instance, created, **kwargs):
-    if created:
-        ServiceUserProfile.objects.create(user=instance)
